cd858d42b025abe6f72cd0e8578c2ba3a90f8448
신성욱 <jshin0627j@gmail.com>
2023-02-21 19:13:20 +0900

    commit_MulticampusUTF8JavaBegin _230221
    LambdaTest2

---
+++ R2_11_LambdaTest2.java
@@ -0,0 +1,34 @@
+package day11;
+/*함수형 인터페이스를 선언하세요
+ * - 문자열 2개를 매개변수로 받으면 두 문자열을 연결하여 반환하는 메서드로 만들려고 한다
+ * 인터페이스명: StringConcat
+ * 추상메서드명: makeString(s1, s2)
+ * - 람다식 이용해서 구현해보가
+ * */
+@FunctionalInterface
+interface StringConcat{
+	String makeString(String s1, String s);
+}
+public class R2_11_LambdaTest2 {
+
+	public static void main(String[] args) {
+		int local=100;//지역변수
+		StringConcat sc=(s1, s2)->{
+			//local=200; [x]
+			//지역변수는 람다식 내부에서 변경하면 에러 남. final변수 이므로
+			System.out.println("local: "+local);
+			return s1+" "+s2;
+		};
+		System.out.println(sc.makeString("Hello", "Lambda"));
+		
+		StringConcat sc2=(x,y)->{
+			return x.toUpperCase()+" "+y.toUpperCase();
+		};
+		System.out.println(sc2.makeString("hi", "java"));
+
+		StringConcat sc3=(a,b)-> a.toLowerCase()+" "+b.toLowerCase();
+		
+		System.out.println(sc3.makeString("Good", "Bye"));
+	}
+
+}
